fd06641cb55425288816190d63697accc032334f
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2018-11-07 15:30:25 +0100

    [BUILD] Prepare repository for automated docker cloud builds
    * Merge ci_build and gradle_alpine in order to create independent images
    * Remove travis build config

--- build.sh
+++ build.sh
@@ -1,18 +1,15 @@
 #!/bin/bash -e
+# Script for local docker build the productive image is created by docker cloud
 
 project_name="saros"
+version="test"
 
-gradle_tag="$project_name/gradle_alpine:openjdk-8"
-
-version="0.2"
 ci_build_tag="$project_name/ci_build:$version"
 stf_test_master_tag="$project_name/stf_test_master:$version"
 stf_test_slave_tag="$project_name/stf_test_slave:$version"
 stf_xmpp_server_tag="$project_name/stf_xmpp_server:$version"
 
 echo "> Building images for build and unit test process"
-echo ">> Build base gradle image"
-docker build --tag "$gradle_tag" -f "Dockerfile.gradle_alpine" .
 echo ">> Build ci build image"
 docker build --tag "$ci_build_tag" -f "Dockerfile.ci_build" .
 
