4cc3600141b0e7cc6b1cd9fdf4b6877f8b043bba
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2018-12-07 09:04:39 +0100

    [BUILD] Change eclipse neon mirror url

--- src/uri_env.sh
+++ src/uri_env.sh
@@ -1,4 +1,4 @@
-export ECLIPSE_URI="http://mirror.switch.ch/eclipse/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-linux-gtk-x86_64.tar.gz" \
+export ECLIPSE_URI="https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/3/eclipse-java-neon-3-linux-gtk-x86_64.tar.gz&mirror_id=1" \
        INTELLIJ_URI="https://download.jetbrains.com/idea/ideaIC-2017.3.5-no-jdk.tar.gz" \
        GRADLE_URI="https://services.gradle.org/distributions/gradle-4.10.1-bin.zip" \
        NODE_URI="https://nodejs.org/dist/v8.0.0/node-v8.0.0-linux-x64.tar.gz"
